c0259e94c54f9fa9c6aee8a624b038829b42cbff
alyssachen04 <alyssachen04@gmail.com>
2024-03-12 23:30:10 -0400

    Added gravity to the player & fixed colliders

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -11,7 +11,10 @@ public class PlayerController : MonoBehaviour
     */
     private Rigidbody rb;
     public float moveSpeed;
-    Vector3 input;
+    public float gravity = 9.81f;
+    public float airControl = 10;
+
+    Vector3 input, moveDirection;
     CharacterController controller;
     Light flashlight;
     bool flashlightOn;
@@ -28,28 +31,45 @@ public class PlayerController : MonoBehaviour
  
     void Update()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
+        if (!LevelManager.isGameOver) {
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
 
-        input = (transform.right * moveHorizontal + transform.forward * moveVertical).normalized;
-        input *= moveSpeed;
-        
-        controller.Move(input * Time.deltaTime);
-
-        if (Input.GetKey(KeyCode.E)) {
-            Debug.Log("Interact!");
-        }
-
-        if (Input.GetKey(KeyCode.C)) {
-            Debug.Log("Hide");
-        }
-
-        if (Input.GetKey(KeyCode.F)) {
-            Debug.Log("Flashlight");
-            if (flashlightOn) {
-                flashlight.intensity = 0;
+            input = (transform.right * moveHorizontal + transform.forward * moveVertical).normalized;
+            input *= moveSpeed;
+            
+            if (controller.isGrounded) {
+               moveDirection = input;
+                /*
+                if (Input.GetButton("Jump")) {
+                    moveDirection.y = Mathf.Sqrt(2 * jumpHeight * gravity);
+                } else {
+                    moveDirection.y = 0.0f;
+                }
+                */
             } else {
-                flashlight.intensity = 1;
+                input.y = moveDirection.y;
+                moveDirection = Vector3.Lerp(moveDirection, input, airControl * Time.deltaTime);
+            }
+
+            moveDirection.y -= gravity * Time.deltaTime;
+            controller.Move(input * Time.deltaTime);
+
+            if (Input.GetKey(KeyCode.E)) {
+                Debug.Log("Interact!");
+            }
+
+            if (Input.GetKey(KeyCode.C)) {
+                Debug.Log("Hide");
+            }
+
+            if (Input.GetKey(KeyCode.F)) {
+                Debug.Log("Flashlight");
+                if (flashlightOn) {
+                    flashlight.intensity = 0;
+                } else {
+                    flashlight.intensity = 1;
+                }
             }
         }
     }
