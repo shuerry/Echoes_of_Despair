1be50137759f8b510057aebf4e5f5a0e436909e5
alyssachen04 <alyssachen04@gmail.com>
2024-03-18 00:32:24 -0700

    Staircase enemy in

--- Assets/Scripts/StairCaseScript.cs
+++ Assets/Scripts/StairCaseScript.cs
@@ -7,10 +7,15 @@ public class StairCaseScript : MonoBehaviour
     Transform player;
     public AudioClip doorSFX;
     bool soundPlayed;
+    bool spawnedYet;
+    public GameObject enemySpawner;
+    public GameObject enemyPrefab;
+
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
         soundPlayed = false;
+        spawnedYet = false;
     }
 
     // Update is called once per frame
@@ -29,5 +34,11 @@ public class StairCaseScript : MonoBehaviour
             
             Debug.Log("Door open!");
         }
+
+         if (spawnedYet == false && Vector3.Distance(player.transform.position, enemySpawner.transform.position) < 2.1) {
+            GameObject enemy = Instantiate(enemyPrefab, enemySpawner.transform.position, enemySpawner.transform.rotation) as GameObject;
+            Debug.Log("EnemySpawned");
+            spawnedYet = true;
+         }
     }
 }
