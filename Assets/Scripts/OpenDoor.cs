e61b7d52fbccb10c85dacb74289735ce4dca0d46
alyssachen04 <alyssachen04@gmail.com>
2024-03-25 21:15:04 -0400

    Updated Player Camera and Door SFX

--- Assets/Scripts/OpenDoor.cs
+++ Assets/Scripts/OpenDoor.cs
@@ -6,11 +6,14 @@ public class OpenDoor : MonoBehaviour
 {
     Transform player;
     public AudioClip doorSFX;
+    public AnimationClip doorSwingAnim;
+    AudioSource audioSource;
     bool soundPlayed;
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
         soundPlayed = false;
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -20,13 +23,21 @@ public class OpenDoor : MonoBehaviour
             (Vector3.Distance(player.position, transform.position) < 2)) 
         {
             gameObject.GetComponent<Animator>().SetTrigger("openDoor");
-
             if (!soundPlayed) {
-                AudioSource.PlayClipAtPoint(doorSFX, transform.position);
+                audioSource.Play();
+                StartCoroutine(PlaySound());
                 soundPlayed = true;
             }
             
+            
             Debug.Log("Door open!");
         }
+    }
+
+    IEnumerator PlaySound()
+    {
+        yield return new WaitForSecondsRealtime(doorSwingAnim.length);
+        audioSource.Stop();
+        Debug.Log("audio stop");
     }
 }
