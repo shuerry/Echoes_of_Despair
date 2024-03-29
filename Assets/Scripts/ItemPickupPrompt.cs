d74101748d6e010a2102b901e8a368cc511e9971
alyssaybarra <alyssa.ybarra014@gmail.com>
2024-03-29 14:56:10 -0400

    item prompt

---
+++ Assets/Scripts/ItemPickupPrompt.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class ItemPickupPrompt : MonoBehaviour
+{
+    [SerializeField]
+    private Text pickUpText;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pickUpText.gameObject.SetActive(false);
+        
+    }
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            pickUpText.gameObject.SetActive(true);
+            Invoke("Disable", 5);
+
+
+           
+        }
+        
+    }
+
+
+
+    private void Disable()
+    {
+        pickUpText.gameObject.SetActive(false); 
+    }
+
+}
