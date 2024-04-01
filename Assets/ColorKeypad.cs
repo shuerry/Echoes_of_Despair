8054c07f5be0e627777fc4fba8750905549de39a
alyssaybarra <alyssa.ybarra014@gmail.com>
2024-03-31 20:02:37 -0400

    completed puzzle

---
+++ Assets/ColorKeypad.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ColorKeypad : MonoBehaviour
+{
+    public Text answer;
+    public Animator door;
+    //public AudioClip doorSFX;
+    public AnimationClip doorSwingAnim;
+    AudioSource audioSource;
+    //public AudioClip buttonSFX;
+    public GameObject doorObject;
+    public Canvas canvas;
+    bool soundPlayed;
+
+
+    private string codeAnswer = "1369485271";
+
+    void Start()
+    {
+
+        audioSource = GetComponent<AudioSource>();
+    }
+    public void Number(int number)
+    {
+        answer.text += number.ToString();
+
+        //AudioSource.PlayClipAtPoint(buttonSFX, transform.position);
+    }
+
+    public void Execute()
+    {
+        //  AudioSource.PlayClipAtPoint(buttonSFX, transform.position);
+        if (answer.text == codeAnswer)
+        {
+
+            answer.text = "CORRECT";
+            door.SetBool("doorOpen", true);
+
+
+            gameObject.GetComponent<Animator>().SetTrigger("openDoor");
+
+            audioSource.Play();
+            StartCoroutine(PlaySound());
+
+
+            canvas.enabled = false;
+        }
+        else
+        {
+            answer.text = "WRONG";
+
+            answer.text = "";
+
+        }
+    }
+
+
+    IEnumerator PlaySound()
+    {
+        yield return new WaitForSecondsRealtime(doorSwingAnim.length);
+        audioSource.Stop();
+        Debug.Log("audio stop");
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        canvas.enabled = true;
+    }
+}
+
---
+++ Assets/PlayAudioPuzzle.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAudioPuzzle : MonoBehaviour
+{
+
+
+         AudioSource puzzleAudio;
+
+
+        // Start is called before the first frame update
+        void Start()
+        {
+        puzzleAudio = GetComponent<AudioSource>();
+       // puzzleAudio.gameObject.SetActive(false);
+
+        }
+
+
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+            puzzleAudio.Play();
+           // puzzleAudio.gameObject.SetActive(true);
+            
+              //  Invoke("Disable", 5);
+
+
+
+            }
+
+        }
+
+
+
+        private void Disable()
+        {
+        puzzleAudio.gameObject.SetActive(false);
+        }
+
+    }
