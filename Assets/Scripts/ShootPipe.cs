28eb63ecbdcd32bcc1890e2829112cf1202bbc49
alyssachen04 <alyssachen04@gmail.com>
2024-04-04 15:04:51 -0400

    Rescaled bathroom assets, added reticle and enemy tag

--- Assets/Scripts/ShootPipe.cs
+++ Assets/Scripts/ShootPipe.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShootPipe : MonoBehaviour
 {
     public GameObject projectilePrefab;
    
     public float projectileSpeed = 100;
+    public Image reticleImage;
    
     void Start()
     {
@@ -28,6 +30,36 @@ public class ShootPipe : MonoBehaviour
             projectile.transform.SetParent(GameObject.FindGameObjectWithTag("PipeParent").transform);
 
        
+        }
+    }
+
+    private void FixedUpdate() {
+        ReticleEffect();
+    }
+
+    void ReticleEffect() {
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position, transform.forward, out hit, Mathf.Infinity)) {
+            if (hit.collider.CompareTag("Enemy")) {
+                reticleImage.color = Color.Lerp
+                    (reticleImage.color, Color.red, Time.deltaTime * 2);
+            
+                reticleImage.transform.localScale = Vector3.Lerp
+                    (reticleImage.transform.localScale, new Vector3(0.7f, 0.7f, 1), Time.deltaTime * 2);
+
+            } else {                
+                reticleImage.color = Color.Lerp
+                    (reticleImage.color, Color.white, Time.deltaTime * 2);
+            
+                reticleImage.transform.localScale = Vector3.Lerp
+                    (reticleImage.transform.localScale, Vector3.one, Time.deltaTime * 2);
+            }
+        } else {
+                reticleImage.color = Color.Lerp
+                    (reticleImage.color, Color.white, Time.deltaTime * 2);
+            
+                reticleImage.transform.localScale = Vector3.Lerp
+                    (reticleImage.transform.localScale, Vector3.one, Time.deltaTime * 2);
         }
     }
 
